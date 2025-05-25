d483960a91d3624a898f65c5f6da0953d12bf452
Gustavo Gabriel <gugagabriel56@gmail.com>
2025-05-25 20:56:37 -0300

    Feat: First accounts layout

--- src/app/components/accounts/accounts.component.ts
+++ src/app/components/accounts/accounts.component.ts
@@ -1,11 +1,44 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-accounts',
-  imports: [],
+  imports: [
+    CommonModule,
+    MatIconModule
+  ],
   templateUrl: './accounts.component.html',
   styleUrl: './accounts.component.css'
 })
 export class AccountsComponent {
-
+  accounts = [
+    {
+      id: 1,
+      description: 'Conta principal',
+      bank: 'Banco do Brasil',
+      accountType: 'Corrente',
+      balance: 1500.00,
+      closingDay: 3,
+      paymentDueDay: 10,
+    },
+    {
+      id: 2,
+      description: 'Conta de poupança',
+      bank: 'Caixa Econômica Federal',
+      accountType: 'Poupança',
+      balance: 5000.00,
+      closingDay: 5,
+      paymentDueDay: 15,
+    },
+    {
+      id: 3,
+      description: 'Cartão de crédito',
+      bank: 'Itaú',
+      accountType: 'Crédito',
+      balance: -200.00,
+      closingDay: 10,
+      paymentDueDay: 20,
+    }
+  ]
 }
