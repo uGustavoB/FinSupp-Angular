8ea9d95c43b67211c83b73ed79aa417d0a646893
Gustavo Gabriel <gugagabriel56@gmail.com>
2025-06-12 21:54:26 -0300

    Refactor: uses ApiService for HTTP calls on Categories

--- src/app/components/categories/categories.component.ts
+++ src/app/components/categories/categories.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { MatIconModule } from '@angular/material/icon';
 import { DeleteModalComponent } from '../util/delete-modal/delete-modal.component';
+import { CategoriesService, Category } from '../../services/categories/categories.service';
 
 @Component({
   selector: 'app-categories',
@@ -14,42 +15,19 @@ import { DeleteModalComponent } from '../util/delete-modal/delete-modal.componen
   styleUrl: './categories.component.css'
 })
 export class CategoriesComponent {
-  categories = [
-    {
-      id: 1,
-      description: 'Comida'
-    },
-    {
-      id: 2,
-      description: 'Transporte'
-    },
-    {
-      id: 3,
-      description: 'Entretenimento'
-    },
-    {
-      id: 4,
-      description: 'Saúde'
-    },
-    {
-      id: 5,
-      description: 'Educação'
-    },
-    {
-      id: 6,
-      description: 'Moradia'
-    },
-    {
-      id: 7,
-      description: 'Lazer'
-    },
-    {
-      id: 8,
-      description: 'Outros'
-    }
-  ];
+  categories: Category[] = [];
 
-  constructor(private dialog: MatDialog) { }
+  constructor(
+    private categoriesService: CategoriesService,
+    private dialog: MatDialog
+  ) { }
+
+  ngOnInit(): void {
+    this.categoriesService.getCategories().subscribe({
+      next: (data) => this.categories = data,
+      error: (err) => console.error('Erro ao buscar categorias', err)
+    });
+  }
 
   openDeleteModal(): void {
     const dialogRef = this.dialog.open(DeleteModalComponent,
