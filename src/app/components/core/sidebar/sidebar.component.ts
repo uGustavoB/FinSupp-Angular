158613c6e1b075d504fb0ace28ef1194b323197c
Gustavo Gabriel <gugagabriel56@gmail.com>
2025-06-13 23:47:41 -0300

    Feat: Adding responsiveness to Sidebar

--- src/app/components/core/sidebar/sidebar.component.ts
+++ src/app/components/core/sidebar/sidebar.component.ts
@@ -1,7 +1,9 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, ElementRef, HostListener, ViewChild } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 import { Router } from '@angular/router';
+import { trigger, state, style, transition, animate } from '@angular/animations';
+
 
 @Component({
   selector: 'app-sidebar',
@@ -10,7 +12,18 @@ import { Router } from '@angular/router';
     MatIconModule
   ],
   templateUrl: './sidebar.component.html',
-  styleUrl: './sidebar.component.css'
+  styleUrl: './sidebar.component.css',
+  animations: [
+    trigger('sidebarAnimation', [
+      state('closed', style({
+        transform: 'translateX(-100%)',
+        opacity: 0
+      })),
+      transition('open <=> closed', [
+        animate('300ms ease-in-out')
+      ])
+    ])
+  ],
 })
 export class SidebarComponent {
   links = [
@@ -51,6 +64,30 @@ export class SidebarComponent {
     }
   ];
 
+  navBarOpen = false;
+  screenWidth = window.innerWidth;
+
+  @ViewChild('sidebarRef') sidebar!: ElementRef;
+  @ViewChild('sidebarButtonRef') sidebarButton!: ElementRef;
+
+  ngOnInit() {
+    this.checkScreenSize();
+  }
+
+  @HostListener('window:resize')
+  onResize() {
+    this.screenWidth = window.innerWidth;
+    this.checkScreenSize();
+  }
+
+  checkScreenSize() {
+    if (this.screenWidth >= 768) {
+      this.navBarOpen = true;
+    } else {
+      this.navBarOpen = false;
+    }
+  }
+
   constructor(private Router: Router) {}
 
   redirectTo(route: string) {
@@ -60,5 +97,27 @@ export class SidebarComponent {
   logout() {
     localStorage.removeItem('token');
     this.Router.navigate(['/auth']);
+  }
+
+  openSidebar() {
+    this.navBarOpen = true;
+    this.sidebar.nativeElement.classList.remove('hidden');
+    this.sidebar.nativeElement.classList.add('absolute', 'z-50');
+    this.sidebarButton.nativeElement.classList.add('hidden');
+  }
+
+  @HostListener('document:click', ['$event'])
+  onClickOutside(event: MouseEvent) {
+    if (this.screenWidth < 768) {
+      const clickedInsideSidebar = this.sidebar.nativeElement.contains(event.target);
+      const clickedOnButton = this.sidebarButton.nativeElement.contains(event.target);
+
+      if (this.navBarOpen && !clickedInsideSidebar && !clickedOnButton) {
+        this.navBarOpen = false;
+        this.sidebar.nativeElement.classList.add('hidden');
+        this.sidebar.nativeElement.classList.remove('absolute', 'z-50');
+        this.sidebarButton.nativeElement.classList.remove('hidden');
+      }
+    }
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,13 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 import { provideHttpClient } from '@angular/common/http';
+import { provideAnimations } from '@angular/platform-browser/animations';
 
 bootstrapApplication(AppComponent, {
   providers: [
     ...appConfig.providers,
-    provideHttpClient()
+    provideHttpClient(),
+    provideAnimations()
   ]
 })
   .catch((err) => console.error(err));
