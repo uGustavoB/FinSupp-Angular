2edce891c90b297a70a78feb65e466e6742e9fce
Gustavo Gabriel <gugagabriel56@gmail.com>
2025-11-25 14:57:37 -0300

    Removendo dashboard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -16,7 +16,7 @@ export const routes: Routes = [
     component: MainLayoutComponent,
     children: [
       {
-        path: "dashboard",
+        path: "",
         component: DashboardComponent
       },
       {
